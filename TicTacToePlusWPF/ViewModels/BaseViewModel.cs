3ce3f4e8b3c4045284a6bf92eefaf2e2910766b0
iamnotsnani <zeynallisnani@gmail.com>
2025-05-12 19:59:13 +0400

    Add project files.

---
+++ TicTacToePlusWPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
---
+++ TicTacToePlusWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Input;
+
+namespace TicTacToePlusWPF.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ICommand StartGameCommand { get; set; }
+        public ICommand OpenSettingsCommand { get; set; }
+        public ICommand OpenHelpCommand { get; set; }
+        public ICommand ExitCommand { get; set; }
+
+        public MainViewModel()
+        {
+            StartGameCommand = new RelayCommand(StartGame);
+            OpenSettingsCommand = new RelayCommand(OpenSettings);
+            OpenHelpCommand = new RelayCommand(OpenHelp);
+            ExitCommand = new RelayCommand(Exit);
+        }
+
+        private void StartGame()
+        {
+            // Logic to start the game
+        }
+
+        private void OpenSettings()
+        {
+            // Logic to open settings
+        }
+
+        private void OpenHelp()
+        {
+            // Logic to open help
+        }
+
+        private void Exit()
+        {
+            // Logic to exit the game
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
